b87bb454628be572b51086205aa7fa33a12778f2
Md Abdur Rahman Salman <139158260+ARSalman23@users.noreply.github.com>
2024-10-11 01:55:32 +0600

    Update C. Closest to the Right.cpp

--- Step 1 Solution/C. Closest to the Right.cpp
+++ Step 1 Solution/C. Closest to the Right.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #define endl "\n"
 const int N = 1e5 + 128;
 int a[N];
+
 int32_t main()
 {
     ios_base::sync_with_stdio(0);
@@ -21,16 +22,13 @@ int32_t main()
         while(l <= r)
         {
             int mid = (l + r) / 2;
-            //cout << l << ' ' << r << ' ' << mid << " ";
             if(a[mid] >= x)
             {
                 ans = min(ans, mid);
-                //cout << ans << endl;
                 r = mid - 1;
             }
             else l = mid + 1;
         }
-        //int ans = lower_bound(a, a + n, x) - a;
         if(ans == INT_MAX) ans = n + 1;
         cout << ans << endl;
     }
