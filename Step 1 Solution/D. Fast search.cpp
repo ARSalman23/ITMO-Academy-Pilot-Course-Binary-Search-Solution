00ca6b8416edd9f5add6570467aad350bbdd2eb6
Md Abdur Rahman Salman <139158260+ARSalman23@users.noreply.github.com>
2024-10-11 01:56:14 +0600

    Update D. Fast search.cpp

--- Step 1 Solution/D. Fast search.cpp
+++ Step 1 Solution/D. Fast search.cpp
@@ -8,25 +8,19 @@ int a[N];
 int32_t main()
 {
     ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
-    int n;
-    cin >> n;
+    cin.tie(0); cout.tie(0);
+    int n; cin >> n;
     for(int i = 0; i < n; i++)
     {
         cin >> a[i];
     }
     sort(a, a + n);
-    int q;
-    cin >> q;
+    int q; cin >> q;
     while(q--)
     {
-        int l, r;
-        cin >> l >> r;
+        int l, r; cin >> l >> r;
         int lo = lower_bound(a, a + n, l) - a;
         int up = upper_bound(a, a + n, r) - a;
-
         cout << up - lo << endl;
     }
     return 0;
